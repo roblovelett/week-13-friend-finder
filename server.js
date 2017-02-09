2d223bb5838c6b4bf3b4ff9f58a3ef1ee3059ff0
Rob Lovelett <rlovelett@gmail.com>
2017-02-08 18:57:49 -0600

    init commit

---
+++ server.js
@@ -0,0 +1,27 @@
+/*
+2. Your `server.js` file should require the basic npm packages we've used in
+class: `express`, `body-parser` and `path`.
+*/
+
+var path = require("path");
+var bodyParser = require("body-parser");
+var express = require("express");
+
+var app = express(); // create express server
+var port = process.env.port || 8000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+	type: "application/vnd.api+json"
+}));
+
+require("./app/routing/api-routes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function () {
+	console.log("listening on port: " + port);
+});
