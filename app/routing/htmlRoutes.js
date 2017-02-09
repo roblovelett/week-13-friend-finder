db76698f44ddba7e4f3e66bfcc4aeedadc694871
Rob Lovelett <rlovelett@gmail.com>
2017-02-09 17:53:46 -0600

    changed htmlRoutes "/"

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,7 +6,7 @@ module.exports = function (app) { // Your `htmlRoutes.js` file should include tw
 		res.sendFile(path.join(__dirname + "/../public/survey.html"));
 	});
 	// A default USE route that leads to `home.html` which displays the home page.
-	app.use(function (req, res) {
+	app.use("/", function (req, res) {
 		res.sendFile(path.join(__dirname + "/../public/home.html"));
 	});
 };
