f169be0bc64b1f748d435d1e03f3611a1fa54b04
Rob Lovelett <rlovelett@gmail.com>
2017-02-09 16:16:54 -0600

    post friend data to api-routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,9 +7,11 @@ module.exports = function (app) { //Your `apiRoutes.js` file should contain two 
         res.json(friends);
     });
     /* A POST routes `/api/friends`. This will be used to handle incoming survey results. 
-This route will also be used to handle the compatibility logic.*/
+    This route will also be used to handle the compatibility logic.*/
     app.post("/api/friends", function (req, res) {
         //
+        // The callback is the response of the server. In our case, we set up code in api-routes that "returns" true or false
+    // depending on if a tables is available or not. 
         friends.push(req.body);
     });
 };
